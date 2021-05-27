ac9911b33f42c1ab36e90ce8f6bd22ac7331db9d
brklein <71305166+brklein@users.noreply.github.com>
2021-05-27 19:17:18 -0400

    Add files via upload

---
+++ Front-End-Final/app.js
@@ -0,0 +1,27 @@
+let currentImgIndex = 0
+
+const images = ['images/Dov.png', 'images/Dockmaster.jpg', 'images/Findalis.jpg', 'images/Hound.jpg', 'images/Nia.jpg']
+
+const mainImage= document.getElementById('mainImage')
+
+mainImage.setAttribute('src', images[currentImgIndex])
+
+const next = document.querySelector('.next')
+
+next.addEventListener('click', () => {
+    if (currentImgIndex >= images.length - 1) {
+         currentImgIndex = 0
+    } else
+        currentImgIndex += 1
+    mainImage.setAttribute('src', images[currentImgIndex])
+})
+const prev = document.querySelector('.prev')
+
+prev.addEventListener('click', () => {
+    if (currentImgIndex <= 0) {
+        currentImgIndex = images.length -1
+    } else {
+        currentImgIndex -= 1
+    }
+    mainImage.setAttribute('src', images[currentImgIndex])
+})
